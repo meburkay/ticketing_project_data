f0361de70a334996768efd7cbc44fbd272741a94
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-24 12:47:29 +0300

    modelmapper added to pom, RoleMapper class and its methods added, we create with @Bean Rolemapper object, we use mapper at RoleServiceImpl

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,21 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+    }
+
+    //To use the third party modelmapper we have to create bean by @Bean annotation because we do not have this class we will take and use it and we can not use stereotype because of that.
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,17 +1,40 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    //We did the RoleRepository injection to reach to DB from the RoleRepository.
+    private final RoleRepository roleRepository;
+    //We add this dependency injection to map role entities and dtos.
+    private final RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
     @Override
     public List<RoleDTO> listAllRoles() {
-        return null;
+
+        //We take all the role entities from db but we must convert it with mappers to RoleDto because we must return RoleDTO for ui. Because of that we add to pom an mapper and used it.
+        List<Role> roleList = roleRepository.findAll();
+
+//        return roleList.stream().map(role -> roleMapper.convertToDto(role)).collect(Collectors.toList());
+//        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+
+        return roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+
+
     }
 
     @Override

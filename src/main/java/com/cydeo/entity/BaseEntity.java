d405b04b6d268f15977c4f878804c752e278b3df
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-24 10:58:37 +0300

    we arrange the base, role and user entities.

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,16 +1,20 @@
 package com.cydeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+//When we use @Data in the future there might be error ı do not know why. Because of that we use getter and setter individually.
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;

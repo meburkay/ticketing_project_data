26ac2472b4d278fa8d88bad2fb1c8cb5596ef624
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-24 12:56:15 +0300

    we use @Lazy annotation for correcting a bug.

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -3,6 +3,7 @@ package com.cydeo.converter;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,8 @@ public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     RoleService roleService;
 
-    public RoleDtoConverter(RoleService roleService) {
+    //We add here @Lazy annotation. Because we say that we want you to inject RoleService when we use it not at the start of the build. Because if it try to inject it at the start because the bean is not ready it go in to a cycle and give error. When we use lazy annotation it did not inject it directly and wait for the usage of the converter which is when we save the data at the ui part.
+    public RoleDtoConverter(@Lazy RoleService roleService) {
         this.roleService = roleService;
     }
 

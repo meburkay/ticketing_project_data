0b3752d218f02a32b898607ba38c3ae51749b5a4
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-10-28 16:44:24 +0300

    we arrange the automatic adding datetime and user parts and user create page is completed.

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -19,9 +19,33 @@ public class BaseEntity {
 
     //We add this for arranging the delete operation. We don't want to delete anything from db. Because of that we create this. The deleted users will become true and the ui part will see only the isDeleted = false ones.
     private Boolean isDeleted = false;
+    //this annotations created because when we do operation update it overwrite the data. But when we use this annotations when update they did not affected from it. insert time and insert userid must be initialize for once. it must not change. Because of that we put updatable = false here otherwise it reseted.
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+
+    //this method executed automatically when we save by this annotation
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+
+    //this method executed automatically when we update data by this annotation.
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
+
 
 }
